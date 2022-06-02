7835eec5ade9f333040acf13f202cda70011fd0f
potlitel <potlitel@gmail.com>
2022-06-01 23:01:41 -0400

    Refactorización menor.

--- src/App.jsx
+++ src/App.jsx
@@ -12,61 +12,6 @@ function App() {
   const [audio, setAudio] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
-  /*$(document).ready(function () {
-    var submitIcon = $(".searchbox-icon");
-    var inputBox = $(".searchbox-input");
-    var searchBox = $(".searchbox");
-    var isOpen = false;
-    submitIcon.click(function () {
-      if (isOpen == false) {
-        searchBox.addClass("searchbox-open");
-        inputBox.focus();
-        isOpen = true;
-      } else {
-        searchBox.removeClass("searchbox-open");
-        inputBox.focusout();
-        isOpen = false;
-      }
-    });
-    submitIcon.mouseup(function () {
-      return false;
-    });
-    searchBox.mouseup(function () {
-      return false;
-    });
-    $(document).mouseup(function () {
-      if (isOpen == true) {
-        $(".searchbox-icon").css("display", "block");
-        submitIcon.click();
-      }
-    });
-  });
-  function buttonUp() {
-    var inputVal = $(".searchbox-input").val();
-    inputVal = $.trim(inputVal).length;
-    if (inputVal !== 0) {
-      $(".searchbox-icon").css("display", "none");
-    } else {
-      $(".searchbox-input").val("");
-      $(".searchbox-icon").css("display", "block");
-    }
-  }
-
-  function searchToggle(obj, e) {
-    var container = $(obj).closest(".search-wrapper");
-    if (!container.hasClass("active")) {
-      container.addClass("active");
-      e.preventDefault();
-    } else if (
-      container.hasClass("active") &&
-      $(obj).closest(".input-holder").length == 0
-    ) {
-      container.removeClass("active");
-      // clear input
-      container.find(".search-input").val("");
-    }
-  }*/
-
   const sleep = (milliseconds) => {
     return new Promise((resolve) => setTimeout(resolve, milliseconds));
   };
@@ -140,34 +85,6 @@ function App() {
           </div>
         )}
       </div>
-
-      {/*<div class="container">
-        <form class="searchbox">
-          <input
-            type="search"
-            placeholder="Search......"
-            name="search"
-            class="searchbox-input"
-            onkeyup="buttonUp();"
-            required
-          />
-          <input type="submit" class="searchbox-submit" value="GO" />
-          <span class="searchbox-icon">GO</span>
-        </form>
-      </div>*/}
-      {/*<div class="search-wrapper">
-        <div class="input-holder">
-          <input
-            type="text"
-            class="search-input"
-            placeholder="Type to search"
-          />
-          <button class="search-icon" onClick={(event)=>searchToggle(this,event)}>
-            <span></span>
-          </button>
-        </div>
-        <span class="close" onClick={(event)=>searchToggle(this,event)}></span>
-      </div>*/}
     </div>
   );
 }
