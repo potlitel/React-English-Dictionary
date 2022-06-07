1dbbcab711e1ca04c7a82c8ea8aa4d27f3a6be11
potlitel <potlitel@gmail.com>
2022-06-07 18:18:54 -0400

    Se especifica color de text en dependencia de los resultados de la búsqueda.

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
       setMain(dataJ[0]);
       //console.log(dataJ[0]);
       const url = dataJ[0].phonetics[0].audio;
-      setAudio("audio .. " + url);
+      setAudio(url);
     } else {
       setWord("");
       setMean([]);
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,28 +1,50 @@
 import { useRef } from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
+import { Grid, GridItem, Text } from "@chakra-ui/react";
 
 const Result = ({ mean, main, audio }) => {
   return (
     <>
       <div className="grid">
         <div className="box box4" style={{ textAlign: "center" }}>
-          {main.word ? main.word : <p>No Definitions Found</p>}
+          {/* Word name section */}
         </div>
         <div className="box box4" style={{ textAlign: "center" }}>
+          {/* Audio section */}
+        </div>
+      </div>
+      <Text fontSize="50px" color={main.word ? "black" : "red"} style={{textAlign:"center"}}>
+        {main.word ? main.word : <p>No Definitions Found</p>}
+      </Text>
+      <Grid
+        h="200px"
+        w="90%"
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(5, 1fr)"
+        gap={4}
+      >
+        <GridItem rowSpan={2} colSpan={1} bg="tomato">
           {main.word && audio ? (
-            <AudioPlayer
+            /*<AudioPlayer
               autoPlay
               src={audio}
               onPlay={(e) => console.log("onPlay")}
               // other props here
-            />
+            />*/
+            <audio
+              controls
+              className="color m-4 text-center col-10"
+              src={audio}
+            ></audio>
           ) : (
             <div className="color fs-3 text-center">Audio not found</div>
           )}
-        </div>   
-        
-      </div>
+        </GridItem>
+        <GridItem colSpan={2} bg="papayawhip" />
+        <GridItem colSpan={2} bg="papayawhip" />
+        <GridItem colSpan={4} bg="tomato" />
+      </Grid>
     </>
   );
 };
